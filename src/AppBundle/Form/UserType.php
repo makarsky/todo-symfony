1460921f6ada13737175313e2dac74df73a77087
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2017-01-20 01:11:24 +0300

    Initial commit

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 15.01.17
+ * Time: 22:23
+ */
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => ['label' => 'Password', 'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px'
+                ],],
+                'second_options' => ['label' => 'Repeat Password', 'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px'
+                ],],
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\User',
+        ]);
+    }
+}
