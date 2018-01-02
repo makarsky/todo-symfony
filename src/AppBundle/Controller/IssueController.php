b85879b0c3dc2b5e41b30501414c6da4e9f9300a
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2018-01-02 18:39:45 +0300

    Rename controller

--- src/AppBundle/Controller/IssueController.php
+++ src/AppBundle/Controller/IssueController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class TodoController extends Controller
+class IssueController extends Controller
 {
     /**
      * @Route("/issue/list", name="issue_list")
